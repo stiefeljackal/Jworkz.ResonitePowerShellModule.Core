1d6e31aa2489c17381c235c7541fec37a7241cc9
Stiefel Jackal <stiefel@jackalworkz.net>
2025-08-19 07:29:52 -0500

    chore: add aggressive inlining for get awaiter result

--- Lib/Utilities/TaskExtensions.cs
+++ Lib/Utilities/TaskExtensions.cs
@@ -1,8 +1,15 @@
-﻿namespace Jworkz.ResonitePowerShellModule.Core.Utilities;
+﻿using System.Runtime.CompilerServices;
+
+namespace Jworkz.ResonitePowerShellModule.Core.Utilities;
 
 public static class TaskExtensions
 {
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void GetAwaiterResult(this Task task) => task.GetAwaiter().GetResult();
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static T GetAwaiterResult<T>(this Task<T> task) => task.GetAwaiter().GetResult();
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static T GetAwaiterResult<T>(this ValueTask<T> vTask) => vTask.GetAwaiter().GetResult();
 }
