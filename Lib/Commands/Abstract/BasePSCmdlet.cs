3bc5c7dbf44c1996bd85e52734afba43acec6eb3
Stiefel Jackal <stiefel@jackalworkz.net>
2025-08-19 08:59:51 -0500

    feat(base): add unilog binding to cmdlet if required

--- Lib/Commands/Abstract/BasePSCmdlet.cs
+++ Lib/Commands/Abstract/BasePSCmdlet.cs
@@ -1,15 +1,22 @@
-﻿using System.Management.Automation;
+﻿using System.Collections.Concurrent;
+using System.Management.Automation;
+using Elements.Core;
 
 namespace Jworkz.ResonitePowerShellModule.Core.Commands.Abstract;
 
 using Core.Models;
 using Core.Models.Abstract;
+using Core.Utilities;
 
 /// <summary>
 /// Base class for all cmdlets
 /// </summary>
-public class BasePSCmdlet : PSCmdlet
+public abstract class BasePSCmdlet : PSCmdlet
 {
+    private const int WRITE_OBJ_QUEUE_WAIT_DELAY = 100;
+
+    private static readonly ConcurrentQueue<(string Type, string Message)> WRITE_QUEUE = new();
+
     private bool _hasError = false;
 
     /// <summary>
@@ -57,7 +64,7 @@ public class BasePSCmdlet : PSCmdlet
     {
         try
         {
-        base.EndProcessing();
+            base.EndProcessing();
             CleanUpCmdlet();
         }
         catch (PipelineStoppedException) { throw; }
@@ -124,7 +131,7 @@ public class BasePSCmdlet : PSCmdlet
         }
     }
 
-    public void StartProcessExecution()
+    internal void StartProcessExecution()
     {
         BeginProcessing();
         ProcessRecord();
@@ -136,30 +143,190 @@ public class BasePSCmdlet : PSCmdlet
         base.StopProcessing();
     }
 
-    internal void WriteError(Exception ex, ErrorCategory errorCategory, object? target = null) 
+    /// <summary>
+    /// Logs an error message and writes it to the error stream.
+    /// </summary>
+    /// <remarks>This method creates an <see cref="Exception"/> object using the provided error message and
+    /// writes it to the error stream with an error category of <see cref="ErrorCategory.WriteError"/>.</remarks>
+    /// <param name="errorMsg">The error message to be logged and written.</param>
+    protected void WriteError(string errorMsg)
     {
-        this.WriteError(new ErrorRecord(ex, string.Empty, errorCategory, target));
+        ArgumentNullException.ThrowIfNull(errorMsg, nameof(errorMsg));
+
+        Exception ex = new(errorMsg);
+        WriteError(ex, ErrorCategory.WriteError);
     }
 
+    /// <summary>
+    /// Writes an error record to the error stream based on the provided exception and error category.
+    /// </summary>
+    /// <remarks>This method creates an <see cref="ErrorRecord"/> using the provided exception and error
+    /// category, and writes it to the error stream. The exception's data is augmented with a UTC timestamp to provide
+    /// additional context for the error.</remarks>
+    /// <param name="ex">The exception that describes the error.</param>
+    /// <param name="errorCategory">The category of the error to use to classify the type of issue.</param>
+    /// <param name="target">The object associated with the error, or <see langword="null"/> if no specific target is applicable.</param>
+    protected void WriteError(Exception ex, ErrorCategory errorCategory, object? target = null)
+    {
+        ex.Data["TimeStampUtc"] = DateTime.UtcNow;
+
+        ErrorDetails errDetails = new(ex.Message);
+        ErrorRecord errRecord = new(ex, "EXCEPTION", ErrorCategory.WriteError, null);
+        errRecord.ErrorDetails = errDetails;
+
+        WriteError(errRecord);
+    }
+
+    /// <summary>
+    /// Examines the provided exception and determines the appropriate error handling action.
+    /// </summary>
+    /// <remarks>This method sets an internal error state and either throws a new exception or writes the
+    /// error based on the current error action configuration. Ensure that the error action is properly configured
+    /// before invoking this method.</remarks>
+    /// <param name="ex">The exception to be examined.</param>
+    /// <exception cref="PSInvalidOperationException">Thrown if the current error action does not indicate a stopping error.</exception>
     private void ExamineThrownException(Exception ex)
     {
         _hasError = true;
-        var errorMessage = ex.Message;
 
         if (!HasStoppingErrorAction())
         {
-            throw new PSInvalidOperationException(errorMessage);
+            throw new PSInvalidOperationException(ex.Message);
         }
 
         if (!HasIgnoreErrorAction())
         {
-            ex.Data["TimeStampUtc"] = DateTime.UtcNow;
-
-            var errDetails = new ErrorDetails(errorMessage);
-            var errRecord = new ErrorRecord(ex, "EXCEPTION", ErrorCategory.WriteError, null);
-            errRecord.ErrorDetails = errDetails;
-
-            WriteError(errRecord);
+            WriteError(ex, ErrorCategory.WriteError);
         }
+    }
+
+    /// <summary>
+    /// Binds a task to the UniLog system to ensure that log entries are processed while the <see cref="ValueTask{T}"/> is running.
+    /// </summary>
+    /// <remarks>This method ensures that log entries in the UniLog system are processed while the specified
+    /// task is running.  It processes log entries from the write queue and handles them based on their type (e.g.,
+    /// info, warning, error). The method blocks until the task is completed and the write queue is empty.  Use this
+    /// method when you need to ensure that all log entries are handled during the execution of a task.</remarks>
+    /// <typeparam name="T">The type of the result produced by the <see cref="ValueTask{T}"/>.</typeparam>
+    /// <param name="task">The <see cref="ValueTask{T}"/> to bind to the UniLog system.</param>
+    /// <param name="isVerboseEnabled">A value indicating whether verbose logging is enabled.  If <see langword="true"/>, verbose log entries will be
+    /// processed; otherwise, they will be ignored.</param>
+    /// <returns>The original <see cref="ValueTask{T}"/> for further chaining or awaiting.</returns>
+    protected ValueTask<T> BindTaskToUniLog<T>(ValueTask<T> task, bool isVerboseEnabled = false)
+    {
+        BindTaskToUniLog(task.AsTask(), isVerboseEnabled);
+        return task;
+    }
+
+    /// <summary>
+    /// Binds a task to the UniLog system to ensure that log entries are processed while the task is running.
+    /// </summary>
+    /// <remarks>This method ensures that log entries in the UniLog system are processed while the specified
+    /// task is running.  It processes log entries from the write queue and handles them based on their type (e.g.,
+    /// info, warning, error). The method blocks until the task is completed and the write queue is empty.  Use this
+    /// method when you need to ensure that all log entries are handled during the execution of a task.</remarks>
+    /// <typeparam name="T">The type of the task, which must derive from <see cref="Task"/>.</typeparam>
+    /// <param name="task">The task to bind to the UniLog system.</param>
+    /// <param name="isVerboseEnabled">A value indicating whether verbose logging is enabled.  If <see langword="true"/>, verbose log entries will be
+    /// processed; otherwise, they will be ignored.</param>
+    /// <returns>The original task for further chaining or awaiting.</returns>
+    protected T BindTaskToUniLog<T>(T task, bool isVerboseEnabled = false) where T : Task
+    {
+        ArgumentNullException.ThrowIfNull(task, nameof(task));
+
+        BindToUniLog(isVerboseEnabled);
+        while (!task.IsCompleted || WRITE_QUEUE.Count > 0)
+        {
+            if (WRITE_QUEUE.TryDequeue(out var entry))
+            {
+                switch (entry.Type)
+                {
+                    case "info":
+                        WriteVerbose(entry.Message);
+                        break;
+                    case "warning":
+                        WriteWarning(entry.Message);
+                        break;
+                    case "error":
+                        WriteError(entry.Message);
+                        break;
+                }
+            }
+            else
+            {
+                Task.Delay(WRITE_OBJ_QUEUE_WAIT_DELAY).GetAwaiterResult();
+            }
+        }
+
+        UnbindFromUniLog();
+
+        return task;
+    }
+
+    /// <summary>
+    /// Binds event handlers to the UniLog logging system to process log messages from it.
+    /// </summary>
+    /// <remarks>This method attaches handlers to the <c>UniLog.OnWarning</c> and <c>UniLog.OnError</c> events
+    /// to process warning and error messages. If <paramref name="includeVerbose"/> is  <see
+    /// langword="true"/>, the handler for verbose log messages is also attached to  <c>UniLog.OnLog</c>. Otherwise, the
+    /// handler for verbose log messages is detached.</remarks>
+    /// <param name="includeVerbose">A value indicating whether verbose log messages should be included.  <see langword="true"/> to include verbose
+    /// log messages; otherwise, <see langword="false"/>.</param>
+    protected static void BindToUniLog(bool includeVerbose)
+    {
+        UniLog.OnWarning += EnqueueWarning;
+        UniLog.OnError += EnqueueError;
+
+        if (includeVerbose)
+        {
+            UniLog.OnLog += EnqueueInfo;
+        }
+        else
+        {
+            UniLog.OnLog -= EnqueueInfo;
+        }
+    }
+
+    /// <summary>
+    /// Unsubscribes from the UniLog event handlers for info, warning, and error messages.
+    /// </summary>
+    /// <remarks>This method detaches the current handlers from the <see cref="UniLog.OnWarning"/>,  <see
+    /// cref="UniLog.OnError"/>, and <see cref="UniLog.OnLog"/> events to stop processing log messages through
+    /// these handlers.</remarks>
+    protected static void UnbindFromUniLog()
+    {
+        UniLog.OnWarning -= EnqueueWarning;
+        UniLog.OnError -= EnqueueError;
+        UniLog.OnLog -= EnqueueInfo;
+    }
+
+    /// <summary>
+    /// Adds an informational message to the write queue.
+    /// </summary>
+    /// <remarks>The message is enqueued with a severity level of "info".</remarks>
+    /// <param name="message">The informational message to enqueue.</param>
+    private static void EnqueueInfo(string message)
+    {
+        WRITE_QUEUE.Enqueue(("info", message));
+    }
+
+    /// <summary>
+    /// Adds a warning message to the write queue.
+    /// </summary>
+    /// <remarks>The message is enqueued with a severity level of "warning".</remarks>
+    /// <param name="message">The warning message to enqueue.</param>
+    private static void EnqueueWarning(string message)
+    {
+        WRITE_QUEUE.Enqueue(("warning", message));
+    }
+
+    /// <summary>
+    /// Adds a error message to the write queue.
+    /// </summary>
+    /// <remarks>The message is enqueued with a severity level of "error".</remarks>
+    /// <param name="message">The error message to enqueue.</param>
+    private static void EnqueueError(string message)
+    {
+        WRITE_QUEUE.Enqueue(("error", message));
     }
 }
